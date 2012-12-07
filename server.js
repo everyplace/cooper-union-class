1c37ec5d2fba16d43e56f12e33f7520bd8787253
806-17 <8o617@41cs806-17.cooper.edu>
2012-12-06 19:57:27 -0500

    adding config object

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ var app = express();
 
 app.get('/', function(req, res){
 
+	var config = {};
+	config.consumer_key = req.query['consumer_key'];
+	config.consumer_secret = req.query['consumer_secret'];
+	config.access_token_key = req.query['access_token_key'];
+	config.access_token_secret = req.query['access_token_secret'];
+		
 	var twit = new twitter({
-		consumer_key: req.query['consumer_key'],
-		consumer_secret: req.query['consumer_secret'],
-		access_token_key: req.query['access_token_key'],
-		access_token_secret: req.query['access_token_secret']
+		consumer_key: config.consumer_key,
+		consumer_secret: config.consumer_secret,
+		access_token_key: config.access_token_key,
+		access_token_secret: config.access_token_secret
 	});
+	
+	console.log(config);
 	
 	var tweetID = req.query['tweetID'];
 	twit.get('/statuses/retweets/'+tweetID+'.json', {include_entities:true}, function(data) {
